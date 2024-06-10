e3c2d78cabe7bbc7c0d64b898234a88b8b44fcfc
hari2596 <>
2024-06-10 16:48:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SpellCheck from './spelllcheck';
+
+function App() {
+  return (
+    <div className="App">
+     <SpellCheck></SpellCheck>
+    </div>
+  );
+}
+
+export default App;
